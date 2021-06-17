73df1af265ad69d6b81f04b2fa0456c3a68926a7
Rodrigo santts <RodrigoSantts.r@gmail.com>
2021-06-17 14:58:12 -0300

    Validação de formulario/ verificação no banco de dados.

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -9,14 +9,49 @@ use MF\Model\Container;
 class IndexController extends Action {
 
 	public function index() {
-
+      
 		$this->render('index');
 	}
 
 	public function inscreverse() {
+      // $this->view->usuario = array(
+      //    'nome' => $_POST[''],
+      //    'email' => $_POST[''],
+      //    'senha' => $_POST[''],
+      // );
+      $this->view->erroCadastro = false;
+      $this->view->erroUsuario = false;
 		$this->render('inscreverse');
 	}
 
+   public function registrar(){
+
+      $usuario = Container::getModel('Usuario');
+      $usuario->__set('nome', $_POST['nome']);
+      $usuario->__set('email', $_POST['email']);
+      $usuario->__set('senha', $_POST['senha']);
+
+      if ($usuario->validarCadastro()){
+         if (count($usuario->getUsuarioPorEmail()) === 0){
+
+            $usuario->salvar();
+            $this->render('cadastro');
+         }else{
+            $this->view->erroUsuario = true;
+            $this->render('inscreverse');
+         }
+      }else {
+         $this->view->erroCadastro = true;
+         $this->view->usuario = array(
+            'nome' => $_POST['nome'],
+            'email' => $_POST['email'],
+            'senha' => $_POST['senha'],
+         );
+         $this->render('inscreverse');
+         echo 'erro';
+      }
+     
+   }
 }
 
 
