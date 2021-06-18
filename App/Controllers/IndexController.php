f93d22112ed5c1db9aecc670db229497e0faee3a
Rodrigo santts <RodrigoSantts.r@gmail.com>
2021-06-18 15:36:22 -0300

    Controle de acesso terminado / Pagína home iniciada.

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,23 +2,16 @@
 
 namespace App\Controllers;
 
-//os recursos do miniframework
 use MF\Controller\Action;
 use MF\Model\Container;
 
 class IndexController extends Action {
 
 	public function index() {
-      
 		$this->render('index');
 	}
 
 	public function inscreverse() {
-      // $this->view->usuario = array(
-      //    'nome' => $_POST[''],
-      //    'email' => $_POST[''],
-      //    'senha' => $_POST[''],
-      // );
       $this->view->erroCadastro = false;
       $this->view->erroUsuario = false;
 		$this->render('inscreverse');
@@ -29,16 +22,17 @@ class IndexController extends Action {
       $usuario = Container::getModel('Usuario');
       $usuario->__set('nome', $_POST['nome']);
       $usuario->__set('email', $_POST['email']);
-      $usuario->__set('senha', $_POST['senha']);
+      $usuario->__set('senha', md5($_POST['senha']));
 
-      if ($usuario->validarCadastro()){
-         if (count($usuario->getUsuarioPorEmail()) === 0){
-
+      if ($usuario->validarCadastro()) {
+         if (count($usuario->getUsuarioPorEmail() ) === 0){
             $usuario->salvar();
             $this->render('cadastro');
+
          }else{
             $this->view->erroUsuario = true;
             $this->render('inscreverse');
+            
          }
       }else {
          $this->view->erroCadastro = true;
@@ -48,9 +42,8 @@ class IndexController extends Action {
             'senha' => $_POST['senha'],
          );
          $this->render('inscreverse');
-         echo 'erro';
       }
-     
+      
    }
 }
 
